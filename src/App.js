90305b9b10a4054a7ecdcba2f0b57bd0702d6e35
Ahmet <ahmt9417@gmail.com>
2021-10-18 15:30:24 +0300

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in Microverse
         </a>
       </header>
     </div>
