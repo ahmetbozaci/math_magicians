e525c1c6742ae6ba795938f15def2ac4f8eae311
Ahmet <ahmt9417@gmail.com>
2021-10-19 16:04:51 +0300

    Add calculator table

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,9 +1,47 @@
+/** @format */
+
 import React from 'react';
+import { Table } from 'reactstrap';
 
 export default class Calculator extends React.PureComponent {
   render() {
     return (
-      <div>SOmething</div>
+      <Table bordered className="mx-auto w-25 text-center">
+        <tbody>
+          <tr>
+            <td className="bg-secondary text-" colSpan="4">Result</td>
+          </tr>
+          <tr>
+            <td className="bg-light">AC</td>
+            <td className="bg-light">+/-</td>
+            <td className="bg-light">%</td>
+            <td className="bg-warning">÷</td>
+          </tr>
+          <tr>
+            <td className="bg-light">7</td>
+            <td className="bg-light">8</td>
+            <td className="bg-light">9</td>
+            <td className="bg-warning">x</td>
+          </tr>
+          <tr>
+            <td className="bg-light">4</td>
+            <td className="bg-light">5</td>
+            <td className="bg-light">6</td>
+            <td className="bg-warning">-</td>
+          </tr>
+          <tr>
+            <td className="bg-light">1</td>
+            <td className="bg-light">2</td>
+            <td className="bg-light">3</td>
+            <td className="bg-warning">+</td>
+          </tr>
+          <tr>
+            <td className="bg-light" colSpan="2">0</td>
+            <td className="bg-light">.</td>
+            <td className="bg-warning">=</td>
+          </tr>
+        </tbody>
+      </Table>
     );
   }
 }
