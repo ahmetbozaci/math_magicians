bf77f3b3545b0fe37fb887efbde3c82acbaa63d5
Ahmet <ahmt9417@gmail.com>
2021-10-22 16:43:51 +0300

    Refactor calculator and app components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Calculator from './components/Calculator';
+import Calculator1 from './components/Calculator1';
 
-export default class App extends React.PureComponent {
-  render() {
-    return (
-      <div><Calculator /></div>
-    );
-  }
+export default function App() {
+  return (
+    <>
+      <Calculator1 />
+    </>
+  );
 }
